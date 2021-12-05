f2967e8edbc183954f164290deb9243732411a07
Mraedis <chaos.side@gmail.com>
2021-12-05 19:25:25 +0100

    Day 5 final revision

--- Day 05/Day 05.1.py
+++ Day 05/Day 05.1.py
@@ -1,14 +1,6 @@
-linelist = [[line.strip().split(' -> ')[0].split(','), line.strip().split(' -> ')[1].split(',')] for line in open('Day 05.input')]
-pairs = []
-for i in range(0, len(linelist)):
-    x0 = linelist[i][0][1]
-    y0 = linelist[i][0][0]
-    x1 = linelist[i][1][1]
-    y1 = linelist[i][1][0]
-    pairs.append([[int(x0), int(y0)], [int(x1), int(y1)]])
-
 vals = {}
-for (x0, y0), (x1, y1) in pairs:
+for line in open('Day 05.input'):
+    x0, y0, x1, y1 = [int(c) for p in line.split(' -> ') for c in p.split(',')]
     if x0 == x1:
         for i in range(min(y0, y1), max(y0, y1)+1):
             vals[x0, i] = vals.get((x0, i), 0) + 1
@@ -16,5 +8,4 @@ for (x0, y0), (x1, y1) in pairs:
         for i in range(min(x0, x1), max(x0, x1)+1):
             vals[i, y0] = vals.get((i, y0), 0) + 1
 
-intersects = sum(x > 1 for x in vals.values())
-print(intersects)
+print(sum(x > 1 for x in vals.values()))
--- Day 05/Day 05.2.py
+++ Day 05/Day 05.2.py
@@ -1,14 +1,6 @@
-linelist = [[line.strip().split(' -> ')[0].split(','), line.strip().split(' -> ')[1].split(',')] for line in open('Day 05.input').readlines()]
-pairs = []
-for i in range(0, len(linelist)):
-    x0 = linelist[i][0][1]
-    y0 = linelist[i][0][0]
-    x1 = linelist[i][1][1]
-    y1 = linelist[i][1][0]
-    pairs.append([[int(x0), int(y0)], [int(x1), int(y1)]])
-
 vals = {}
-for (x0, y0), (x1, y1) in pairs:
+for line in open('Day 05.input'):
+    x0, y0, x1, y1 = [int(c) for p in line.split(' -> ') for c in p.split(',')]
     dx = 0 if x1 == x0 else (x1-x0)//abs(x1-x0)
     dy = 0 if y1 == y0 else (y1-y0)//abs(y1-y0)
 
