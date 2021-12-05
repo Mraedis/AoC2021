2dec30d64aa40a1570475e5a752418ef4f835038
Mraedis <chaos.side@gmail.com>
2021-12-05 17:41:15 +0100

    Day 5 touch up

--- Day 05/Day 05.1.py
+++ Day 05/Day 05.1.py
@@ -1,26 +1,20 @@
-linelist = [[line.strip().split(' -> ')[0].split(','), line.strip().split(' -> ')[1].split(',')] for line in open('Day 05.input').readlines()]
+linelist = [[line.strip().split(' -> ')[0].split(','), line.strip().split(' -> ')[1].split(',')] for line in open('Day 05.input')]
 pairs = []
 for i in range(0, len(linelist)):
-    x0 = linelist[i][0][0]
-    y0 = linelist[i][0][1]
-    x1 = linelist[i][1][0]
-    y1 = linelist[i][1][1]
+    x0 = linelist[i][0][1]
+    y0 = linelist[i][0][0]
+    x1 = linelist[i][1][1]
+    y1 = linelist[i][1][0]
     pairs.append([[int(x0), int(y0)], [int(x1), int(y1)]])
 
-grid = [[0 for col in range(1000)] for row in range(1000)]
-for pair in pairs:
-    y0 = pair[0][0]
-    x0 = pair[0][1]
-    y1 = pair[1][0]
-    x1 = pair[1][1]
+vals = {}
+for (x0, y0), (x1, y1) in pairs:
     if x0 == x1:
         for i in range(min(y0, y1), max(y0, y1)+1):
-            grid[x0][i] += 1
+            vals[x0, i] = vals.get((x0, i), 0) + 1
     elif y0 == y1:
         for i in range(min(x0, x1), max(x0, x1)+1):
-            grid[i][y1] += 1
+            vals[i, y0] = vals.get((i, y0), 0) + 1
 
-intersects = 0
-for line in grid:
-    intersects += sum(x > 1 for x in line)
+intersects = sum(x > 1 for x in vals.values())
 print(intersects)
--- Day 05/Day 05.2.py
+++ Day 05/Day 05.2.py
@@ -1,40 +1,22 @@
 linelist = [[line.strip().split(' -> ')[0].split(','), line.strip().split(' -> ')[1].split(',')] for line in open('Day 05.input').readlines()]
 pairs = []
 for i in range(0, len(linelist)):
-    x0 = linelist[i][0][0]
-    y0 = linelist[i][0][1]
-    x1 = linelist[i][1][0]
-    y1 = linelist[i][1][1]
+    x0 = linelist[i][0][1]
+    y0 = linelist[i][0][0]
+    x1 = linelist[i][1][1]
+    y1 = linelist[i][1][0]
     pairs.append([[int(x0), int(y0)], [int(x1), int(y1)]])
 
-grid = [[0 for col in range(1000)] for row in range(1000)]
-for pair in pairs:
-    y0 = pair[0][0]
-    x0 = pair[0][1]
-    y1 = pair[1][0]
-    x1 = pair[1][1]
-    if x0 == x1:
-        for i in range(min(y0, y1), max(y0, y1)+1):
-            grid[x0][i] += 1
-    elif y0 == y1:
-        for i in range(min(x0, x1), max(x0, x1)+1):
-            grid[i][y1] += 1
-    elif x0 < x1:
-        if y0 < y1:
-            for i in range(0, x1-x0+1):
-                grid[x0+i][y0+i] += 1
-        else:
-            for i in range(0, x1-x0+1):
-                grid[x0+i][y0-i] += 1
-    else:  # x1 < x0
-        if y0 < y1:
-            for i in range(0, x0-x1+1):
-                grid[x1+i][y1-i] += 1
-        else:
-            for i in range(0, x0-x1+1):
-                grid[x1+i][y1+i] += 1
+vals = {}
+for (x0, y0), (x1, y1) in pairs:
+    dx = 0 if x1 == x0 else (x1-x0)//abs(x1-x0)
+    dy = 0 if y1 == y0 else (y1-y0)//abs(y1-y0)
 
-intersects = 0
-for line in grid:
-    intersects += sum(x > 1 for x in line)
+    while x1 != x0 or y1 != y0:
+        vals[x0, y0] = vals.get((x0, y0), 0) + 1
+        x0 += dx
+        y0 += dy
+    vals[x0, y0] = vals.get((x0, y0), 0) + 1
+
+intersects = sum(x > 1 for x in vals.values())
 print(intersects)
