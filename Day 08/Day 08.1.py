476269c9e09a37b79e0a21a8f797a391d25c3e50
Mraedis <chaos.side@gmail.com>
2021-12-08 09:53:27 +0100

    Day 8.1 revision

--- Day 08/Day 08.1.py
+++ Day 08/Day 08.1.py
@@ -4,6 +4,5 @@ up = [line.strip().split(' | ')[1] for line in open('Day 08.input')]
 
 total = 0
 for u in up:
-    total += sum(len(x) == 2 for x in u.split(' ')) + sum(len(x) == 3 for x in u.split(' ')) +\
-             sum(len(x) == 4 for x in u.split(' ')) + sum(len(x) == 7 for x in u.split(' '))
+    total += sum(len(x) in [2, 3, 4, 7] for x in u.split(' '))
 print(total)
