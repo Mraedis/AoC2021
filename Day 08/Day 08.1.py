07c9433cfd17ac23666fd5be1095193b61348c55
Mraedis <chaos.side@gmail.com>
2021-12-08 09:54:44 +0100

    Day 8.1 revision

--- Day 08/Day 08.1.py
+++ Day 08/Day 08.1.py
@@ -1,8 +1,2 @@
-sevseg = {0: 'abcefg', 1: 'cf', 2: 'acdeg', 3: 'acdfg', 4: 'bcdf',
-          5: 'abdfg', 6: 'abdefg', 7: 'acf', 8: 'abcdefg', 9: 'abcdfg'}
 up = [line.strip().split(' | ')[1] for line in open('Day 08.input')]
-
-total = 0
-for u in up:
-    total += sum(len(x) in [2, 3, 4, 7] for x in u.split(' '))
-print(total)
+print(sum(len(x) in [2, 3, 4, 7] for u in up for x in u.split(' ')))
